10749edfa6abf46f98800378cb05dc5426914236
Nguyen Hong Son <105625576+Sonrevolt@users.noreply.github.com>
2023-11-06 22:06:38 +0700

    final

---
+++ Assets/Scripts/DamageNumber.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamageNumber : MonoBehaviour
+{
+    public TMP_Text damageText;
+
+    public float lifetime;
+    public float lifeCounter;
+
+    public float floatSpeed = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (lifeCounter > 0)
+        {
+            lifeCounter -= Time.deltaTime;
+
+            if(lifeCounter <= 0)
+            {
+                //Destroy(gameObject);
+
+                DamageNumberController.instance.PlaceInPool(this);
+            }
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.U))
+        {
+            Setup(45);
+        }  */
+
+        transform.position += Vector3.up * floatSpeed * Time.deltaTime;
+    }
+
+    public void Setup(int damageDislay)
+    {
+        lifeCounter = lifetime;
+
+        damageText.text = damageDislay.ToString();
+    }
+}
